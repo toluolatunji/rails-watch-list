5f60cf97ef789a9efb884804bc61071eeb4cbd44
Tolu Olatunji <olatunji.toluid@gmail.com>
2023-05-29 10:09:12 +0100

    add a root path

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,7 +13,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save!
+    if @list.save!
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "lists#index"
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create]
+    resources :bookmarks, only: %i[new create destroy]
   end
-  resources :bookmarks, only: [:destroy]
 end
